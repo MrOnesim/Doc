7e594de5fb7a66a744dc3cc3611f7b1b98b9ed59
MrOnesim <gracaonesim@gmail.com>
2026-03-09 12:03:12 +0100

    Save all file

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/Button";
+
+function App() {
+    return (
+        <div>
+            <h1 className="text-center">Hello, React!</h1>
+            <Button />
+        </div>
+    );
+}
+
+export default App;
